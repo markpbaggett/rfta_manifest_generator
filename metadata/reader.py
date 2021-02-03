7cab416f7521e38e05368060564b3ce0fe1e9db3
Mark Baggett <mbagget1@utk.edu>
2021-02-03 08:32:31 -0500

    Initialize interview.

--- metadata/reader.py
+++ metadata/reader.py
@@ -4,7 +4,8 @@ from csv import DictReader
 class MetadataReader:
     def __init__(self, metadata_file):
         self.filename = metadata_file
-        self.interviews = self.__read_data(metadata_file)
+        self.original_interviews = self.__read_data(metadata_file)
+        self.interviews = self.__clean_interviews()
 
     @staticmethod
     def __read_data(path):
@@ -13,6 +14,34 @@ class MetadataReader:
                 interview
                 for interview in DictReader(my_csv, delimiter="|", quotechar="%")
             ]
+
+    def __clean_interviews(self):
+        return [Interview(interview).metadata for interview in self.original_interviews]
+
+
+class Interview:
+    def __init__(self, interview):
+        self.csv_data = interview
+        self.metadata = self.__generate_interview()
+
+    def get_interview_label(self):
+        """Use Title to generate a label for the Manifest."""
+        return self.csv_data["Title"]
+
+    def get_rights(self):
+        """Use License to generate rights for the Manifest."""
+        return self.csv_data["License"]
+
+    def get_summary(self):
+        """Use Abstract to generate a summary for the manifest."""
+        return self.csv_data["Abstract"]
+
+    def __generate_interview(self):
+        return {
+            "label": self.get_interview_label(),
+            "rights": self.get_rights(),
+            "summary": self.get_summary(),
+        }
 
 
 if __name__ == "__main__":
