d640fa57de0ad372b7a8bf878ff28f6e3c4305a5
Mark Baggett <mbagget1@utk.edu>
2021-02-08 12:52:37 -0500

    Test all data.

--- tests/tests.py
+++ tests/tests.py
@@ -17,47 +17,61 @@ class CheckDescriptiveMetadata(unittest.TestCase):
         self.assertIn("en", self.metadata_element["value"])
         self.assertIsInstance(self.metadata_element["value"]["en"], list)
 
+    def check_geonames_uri_is_not_present(self):
+        self.assertNotIn("https://www.geonames.org/", self.metadata_element["value"]["en"][0])
+
 
 class TestDescriptiveMetadata(unittest.TestCase):
     def test_narrator(self):
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_narrators()
-        ).check_label()
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_narrators()
-        ).check_value()
+        for interview in interview_data:
+            CheckDescriptiveMetadata(
+                Interview(interview).get_narrators()
+            ).check_label()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_narrators()
+            ).check_value()
 
     def test_interviewer(self):
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_interviewer()
-        ).check_label()
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_interviewer()
-        ).check_value()
+        for interview in interview_data:
+            CheckDescriptiveMetadata(
+                Interview(interview).get_interviewer()
+            ).check_label()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_interviewer()
+            ).check_value()
 
     def test_interviewer_location(self):
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_interviewer_location()
-        ).check_label()
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_interviewer_location()
-        ).check_value()
+        for interview in interview_data:
+            CheckDescriptiveMetadata(
+                Interview(interview).get_interviewer_location()
+            ).check_label()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_interviewer_location()
+            ).check_value()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_interviewer_location()
+            ).check_geonames_uri_is_not_present()
 
     def test_narrator_location(self):
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_narrator_location()
-        ).check_label()
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_narrator_location()
-        ).check_value()
+        for interview in interview_data:
+            CheckDescriptiveMetadata(
+                Interview(interview).get_narrator_location()
+            ).check_label()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_narrator_location()
+            ).check_value()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_narrator_location()
+            ).check_geonames_uri_is_not_present()
 
     def test_aat_format(self):
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_aat_format()
-        ).check_label()
-        CheckDescriptiveMetadata(
-            Interview(interview_data).get_aat_format()
-        ).check_value()
+        for interview in interview_data:
+            CheckDescriptiveMetadata(
+                Interview(interview).get_aat_format()
+            ).check_label()
+            CheckDescriptiveMetadata(
+                Interview(interview).get_aat_format()
+            ).check_value()
 
 
 if __name__ == "__main__":
